b827abb3c756b39045753926e7e34dc99c1df5d2
Kevin McIntyre <kmcintyre@greenchargenet.com>
2014-06-25 12:45:35 -0700

    remove bash

--- flag/logo_scrape/fit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for f in *.svg
-do
-	echo `basename $f`	 
-	rsvg-convert `basename $f` -h 20 -f svg -o '../logo/'`basename $f`
-done
--- flag/logo_scrape/rename.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-find . -print | while read FILENAME
-do 
-	rename -v 's/-/_/g' "$FILENAME"
-done
